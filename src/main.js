19ca91c85d3177eabbbfa66c847a851f6395baa3
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-07-10 19:26:25 +0530

    feat: complete desktop design

--- src/main.js
+++ src/main.js
@@ -10,20 +10,17 @@ let split = SplitText.create(".split", { type: "words, chars" });
 
 // now animate the characters in a staggered fashion
 gsap.from(split.chars, {
-  duration: 1, 
-  y: 100,         // animate from 100px below
-  autoAlpha: 0,   // fade in from opacity: 0 and visibility: hidden
-  stagger: 0.05,  // 0.05 seconds between each
+  duration: 1,
+  y: 100, // animate from 100px below
+  autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+  stagger: 0.05, // 0.05 seconds between each
 });
 
-
-
 // Hamburger menu toggle for 'X' animation
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('is-open');
-  
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("is-open");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        blue: 'hsl(198, 62%, 26%)',
-        yellow: 'hsl(51, 100%, 49%)',
-        'green-dark': 'hsl(167, 40%, 24%)',
-        'green-light': 'hsl(167, 34%, 31%)',
-        'green-lighter': 'hsl(167, 34%, 61%)',
-        'navbar-color': 'hsl(200, 100%, 62%)',
+        blue: "hsl(198, 62%, 26%)",
+        yellow: "hsl(51, 100%, 49%)",
+        "green-dark": "hsl(167, 40%, 24%)",
+        "green-light": "hsl(167, 34%, 31%)",
+        "green-lighter": "hsl(167, 34%, 61%)",
+        "navbar-color": "hsl(200, 100%, 62%)",
       },
     },
 
     fontFamily: {
-      barlow: ['Barlow', 'sans-serif'],
-      fraunces: ['Fraunces', 'serif']
-    }
+      barlow: ["Barlow", "sans-serif"],
+      fraunces: ["Fraunces", "serif"],
+    },
   },
   plugins: [],
-}
+};
